cdeca0e51fac84b1e795456a11600deca2e732db
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-08-11 09:17:18 -0300

    Estudando Propiedades em linguagem orientada a objetos

---
+++ dock/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+namespace airforce {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Docks dados = new Docks();
+
+            Console.WriteLine("TELA DE LOGIN");
+            Console.WriteLine();
+            Console.Write("Digite o usuário: ");
+            dados.User = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Digite a senha: ");
+            dados.Password = int.Parse(Console.ReadLine());
+            Console.Write("Digite o ip: ");
+            dados.Ip = int.Parse(Console.ReadLine());
+            Console.Write("Digite o host: ");
+            dados.HostUser = Console.ReadLine();
+
+        }
+        
+    }
+
+}
---
+++ dock/docks.cs
@@ -0,0 +1,71 @@
+﻿
+
+namespace airforce {
+    internal class Docks {
+
+        private string _user;
+        private int _password;
+        public string HostUser {
+            get { return HostUser; }
+            set {
+                Console.WriteLine("..."); 
+            }
+        }
+        public int Ip {
+            get { return Ip; }
+            set {
+                Console.WriteLine("...");
+            } 
+        }
+
+
+        public Docks() { }
+
+        public Docks(string user) {
+            _user = user;
+        }
+
+        public Docks(string user, int password) : this(user) {
+            _password = password;
+        }
+
+        public Docks(string user, int password, int ip) : this(user, password) {
+            Ip = ip;
+            
+        }
+
+        public string User {
+
+            get { return _user; }
+            set {
+                if(value != null && value.Length > 6) {
+                    Console.WriteLine("...");
+                    
+                }
+                else {
+                    Console.WriteLine("...");
+                    Console.WriteLine($"Usuário não aceito");
+                }
+            }
+
+        }
+
+        public int Password {
+
+            get { return _password; }
+
+            set {
+                if(value != null && value > 3) {
+                    Console.WriteLine("...");
+                   
+                }
+                else {
+                    Console.WriteLine("...");
+                    Console.WriteLine("Senha negada");
+                }
+            }
+
+        }
+
+    }
+}
